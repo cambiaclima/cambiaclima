ff48d4b417a796202962a85492e699b0c589d195
cima <63076598+tia-cima@users.noreply.github.com>
2022-02-21 00:57:18 +0100

    first problema nell'apertura dell'articolo solving

--- admin/articoli/add_articolo.php
+++ admin/articoli/add_articolo.php
@@ -1,12 +1,20 @@
 <?php
-  require '../escape.php';
+  require '../escape.php'; 
+
+  //Connesione al server
+  $dbServerName = "localhost";
+  $dbUserName = "cambiaclima5bi";
+  $dbPassword = "";
+  $dbName = "my_cambiaclima5bi";
+  $conn= mysqli_connect($dbServerName,$dbUserName,$dbPassword,$dbName) or die('Unable to establish a SQL connection');
+  $conn->set_charset("utf8");
+
   $titolo=$_POST['titolo'];
   $breve_descr=$_POST['breve_descr'];
   $contenuto=$_POST['contenuto'];
   $immagine= preg_replace('/\s+/', '', basename($_FILES["fileToUpload"]["name"]));
 
-  //Connesione al server
-	require '/www/cambiaclima/includes/dbconnect.php';
+
 
   $sql="INSERT INTO articolo (Titolo,breve_descr,contenuto,immagine,data_pubbli)";
   $sql .="VALUES('$titolo','$breve_descr','$contenuto','$immagine',CURDATE());";
@@ -21,7 +29,19 @@
   $sql="SELECT IDArt FROM articolo where Titolo = '".$titolo."' AND breve_descr = '".$breve_descr."' AND contenuto = '".$contenuto."' AND immagine = '".$immagine."' AND data_pubbli = CURDATE();";
   $result = mysqli_query($conn,$sql);
   $IDArt = mysqli_fetch_array($result)[0];
+  mkdir('/cambiaclima/articoli/'.$IDArt);
+  require 'upload.php';
 
+  // require generate page
+  $myfile = fopen("cambiaclima/admin/articoli/".$IDArt.".php", "w") or die("Unable to open file!");
+  $txt = '<?php $IDArt='.$IDArt.';?>';
+  fwrite($myfile, $txt);
+  $txt = '<?php require "blank_article.php";?>';
+  fwrite($myfile, $txt);
+  fclose($myfile);
+
+  mysqli_close($conn);
+  header("location: ../bacheca.php");
 
 /*include_once '../dballmail.php';
 require("cambiaclima/mail/SendGrid/sendgrid-php.php");
@@ -67,11 +87,8 @@ try {
     //echo 'Caught exception: '. $e->getMessage() ."\n";
 }*/
 
-  mkdir('../articoli/'.$IDArt);
-  require 'upload.php';
-  require 'generate_page.php';
 
-  mysqli_close($conn);
-  header("location: ../bacheca.php");
+
+
 
 ?>
