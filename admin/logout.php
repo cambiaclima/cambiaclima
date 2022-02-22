9b285a46cfdb713c1623f1f11750948c4de20147
cima <63076598+tia-cima@users.noreply.github.com>
2022-02-22 23:04:09 +0100

    added comment about not using cookies

--- admin/logout.php
+++ admin/logout.php
@@ -1,4 +1,4 @@
 <?php
     $fp = fopen("temp/passwordTemp.json", "w"); // SISTEMA LOGIN: PASSO 8
-    fwrite($fp, '');
+    fwrite($fp, ''); // un sistema di login basato su variabili di sessione e coockie era poco efficiente da realizzare e avremmo dovuto chiedere il consenso all'utilizzo dei cookie all'utente
 ?>
