7d6e557c57c7560bdd022b1280f0198ab578ec1d
cima <63076598+tia-cima@users.noreply.github.com>
2022-02-26 19:17:01 +0100

    restored index

--- includes/dbconnect.php
+++ includes/dbconnect.php
@@ -1,11 +1,12 @@
 <?php
-$dbServerName = "localhost";
-$dbUserName = "cambiaclima5bi";
-$dbPassword = "";
-$dbName = "my_cambiaclima5bi";
+$dbServerName = "remotemysql.com";
+$dbUserName = "fGPR7boOmP";
+$dbPassword = "wBx7ZG1Nbd";
+$dbName = "fGPR7boOmP";
+$dbport = 3306;
 
 // ricordarsi di mettere privato il file una volta configurata la connessione remota cosi che non si vedano i parametri di accesso dal repository 
 
-$conn= mysqli_connect($dbServerName,$dbUserName,$dbPassword,$dbName) or die('Unable to establish a SQL connection');
+$conn= mysqli_connect($dbServerName,$dbUserName,$dbPassword,$dbName, $dbport) or die('Unable to establish a SQL connection');
 $conn->set_charset("utf8");
 ?>
--- index.php
+++ index.php
@@ -128,6 +128,6 @@
 		<!-- Footer -->
 		<?php require 'includes/footer.php';?>
 
-
+<!-- //ciao -->
 	</body>
 </html>
