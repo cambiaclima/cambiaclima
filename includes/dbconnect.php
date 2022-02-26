63afcd5c1b8833160280beeb5f0a4477968a2e1b
cima <63076598+tia-cima@users.noreply.github.com>
2022-02-26 19:33:07 +0100

    update dbconnect credentials

--- includes/dbconnect.php
+++ includes/dbconnect.php
@@ -1,12 +1,11 @@
 <?php
-$dbServerName = "remotemysql.com";
-$dbUserName = "fGPR7boOmP";
-$dbPassword = "wBx7ZG1Nbd";
-$dbName = "fGPR7boOmP";
-$dbport = 3306;
+$dbServerName = "localhost";
+$dbUserName = "cambiaclima5bi";
+$dbPassword = "";
+$dbName = "my_cambiaclima5bi";
 
 // ricordarsi di mettere privato il file una volta configurata la connessione remota cosi che non si vedano i parametri di accesso dal repository 
 
-$conn= mysqli_connect($dbServerName,$dbUserName,$dbPassword,$dbName, $dbport) or die('Unable to establish a SQL connection');
+$conn= mysqli_connect($dbServerName,$dbUserName,$dbPassword,$dbName) or die('Unable to establish a SQL connection');
 $conn->set_charset("utf8");
 ?>
