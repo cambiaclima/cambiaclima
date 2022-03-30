5223b345ac88c00728c5fbcc8ee1eb54d3260352
cima <cimadomomattia@gmail.com>
2022-03-31 00:24:12 +0200

    new CO2 calculator

---
+++ source/CO2/calcoloCO2Auto.php
@@ -0,0 +1,25 @@
+<?php require '../../includes/var.php';?>
+<?php
+	$metaTags = "Pagina relativa al calcolo dell'a CO2 prodotta da un singolo individuo" ;
+	$currentPage = 'Calcolo CO2';
+	$aggiuntivo = '<img name="logo" src="../img/img4.jpg" alt="CO2">';
+?>
+<html lang="it">
+<html lang="it">
+	<head>
+	<link rel="stylesheet" href="/cambiaclima/css/styleCo2.css?<?echo filemtime('../css/styleCo2.css');?>">
+			<?php require '../../includes/head.php';?>
+	</head>
+	<body>	
+		<!-- Header -->
+		<?php require '../../includes/header.php';?>
+		<!-- NavBar -->
+		<?php require '../../includes/nav.php';?>
+		<!-- Container -->
+		<main class="container-md shadow-sm" role="main">
+            <?php require 'fun/JSfunction_calcoloCO2_auto.html'?>
+		</main>
+		 <!--Footer -->
+		<?php require '../../includes/footer.php';?>	
+	</body>
+</html>
